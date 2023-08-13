dfc5219e1962de83646af34c369fbbaf67306c34
James Patterson <jamespatterson.feswe@yahoo.com>
2023-08-13 04:20:51 -0400

    run: prettier

--- src/components/header-footer/header-footer.tsx
+++ src/components/header-footer/header-footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import btn from '../../assets/menu-btn.png';
-import './header-footer.scss'
+import btn from "../../assets/menu-btn.png";
+import "./header-footer.scss";
 
 function HeaderFooter({ params }): React.JSX.Element {
   const isHeader = !!params.header;
@@ -8,11 +8,25 @@ function HeaderFooter({ params }): React.JSX.Element {
   return (
     <>
       <section className="flexed tool">
-        <div className={ `${isHeader ? 'menu-btn-container' : ''}` }>
-          { isHeader ? <img className={ `${ isHeader ? 'flexed ' : '' }btn` } src={btn} alt="a menu button" /> : '' }
+        <div className={`${isHeader ? "menu-btn-container" : ""}`}>
+          {isHeader ? (
+            <img
+              className={`${isHeader ? "flexed " : ""}btn`}
+              src={btn}
+              alt="a menu button"
+            />
+          ) : (
+            ""
+          )}
         </div>
-        <div className={`flexed ${isHeader ? 'bar header' : 'footer'}`}>
-          {isHeader ? <><h1>Movie Database</h1></> : '® xRzy'}
+        <div className={`flexed ${isHeader ? "bar header" : "footer"}`}>
+          {isHeader ? (
+            <>
+              <h1>Movie Database</h1>
+            </>
+          ) : (
+            "® xRzy"
+          )}
         </div>
       </section>
     </>
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import './main.scss';
+import "./main.scss";
 import HeaderFooter from "../header-footer/header-footer";
 import Movies from "../movies/movies";
 
 function Main() {
   return (
     <main>
-      <HeaderFooter params={{ header: true }}/>
+      <HeaderFooter params={{ header: true }} />
       <section className="container">
         <Movies />
       </section>
-      <HeaderFooter params={{ footer: true }}/>
+      <HeaderFooter params={{ footer: true }} />
     </main>
   );
 }
