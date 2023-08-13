a2a7ef810b1d438f009c73bdf9ff5467df1e066f
James Patterson <jamespatterson.feswe@yahoo.com>
2023-08-13 01:53:12 -0400

    restructure to allow sass

---
+++ src/components/card/card.tsx
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <main>
+        <header>
+            <h1>Movie Database</h1>
+        </header>
+    </main>
+  );
+}
+
+export default Card;
---
+++ src/components/main/main.tsx
@@ -0,0 +1,15 @@
+function Main() {
+    return (
+      <main>
+          <header className="header">
+              <h1>Movie Database</h1>
+          </header>
+          <section>
+            this is where content will go
+          </section>
+      </main>
+    );
+  }
+  
+  export default Main;
+  
