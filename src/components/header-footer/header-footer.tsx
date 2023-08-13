29fd43ab21a8b8e63065ae65315e4d0e3beb47f4
James Patterson <jamespatterson.feswe@yahoo.com>
2023-08-13 04:12:13 -0400

    add: png menu btn, add styles, logic to use png

--- src/components/header-footer/header-footer.tsx
+++ src/components/header-footer/header-footer.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import btn from '../../assets/menu-btn.png';
 import './header-footer.scss'
 
 function HeaderFooter({ params }): React.JSX.Element {
   const isHeader = !!params.header;
 
   return (
-    <section className={`toolbar ${isHeader ? 'header' : 'footer'}`}>
-      {isHeader ? 'Header' : '® xRzy'}
-    </section>
+    <>
+      <section className="flexed tool">
+        <div className={ `${isHeader ? 'menu-btn-container' : ''}` }>
+          { isHeader ? <img className={ `${ isHeader ? 'flexed ' : '' }btn` } src={btn} alt="a menu button" /> : '' }
+        </div>
+        <div className={`flexed ${isHeader ? 'bar header' : 'footer'}`}>
+          {isHeader ? <><h1>Movie Database</h1></> : '® xRzy'}
+        </div>
+      </section>
+    </>
   );
 }
 
