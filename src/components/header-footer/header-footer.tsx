cbe45505440b5697634d58eae68d03af60142e56
James Patterson <jamespatterson.feswe@yahoo.com>
2023-08-17 19:25:19 -0400

    add: search bar without logic

--- src/components/header-footer/header-footer.tsx
+++ src/components/header-footer/header-footer.tsx
@@ -27,6 +27,16 @@ function HeaderFooter({ params }): React.JSX.Element {
           ) : (
             ""
           )}
+          {/* <div className="input-group mb-3">
+            <input aria-label="movie search input field"
+                    className="form-control"
+                    data-testid="movie-search-input"
+                    onChange={e => handleSearchTerm((e.target.value).toLowerCase())}
+                    placeholder="Search the database of movies"
+                    type="search"
+                    value={searchTerm}>
+            </input>
+          </div> */}
         </div>
         <div className={`flexed ${isHeader ? "bar header" : "footer"}`}>
           {isHeader ? (
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.scss";
 import HeaderFooter from "../header-footer/header-footer";
 import Movies from "../movies/movies";
 
 function Main() {
+  const [ searchTerm, setSearchTerm ] = useState('');
+
+  const handleSearchTerm = (term: string) => {
+      setSearchTerm(term);
+  }
   return (
     <main>
       <HeaderFooter params={{ header: true }} />
+      <div className="input">
+        <input aria-label="movie search input field"
+          data-testid="movie-search-input"
+          onChange={e => handleSearchTerm((e.target.value).toLowerCase())}
+          placeholder="Search the database of movies"
+          type="search"
+          value={searchTerm}>
+        </input>
+      </div>
       <section className="container">
         <Movies />
       </section>
